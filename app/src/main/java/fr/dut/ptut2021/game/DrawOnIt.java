8a75497ff95f60640174e9fb88107e5ceaa534ea
COMTE Axel p2004227 <axel.comte@etu.univ-lyon1.fr>
2021-11-14 01:42:17 +0100

    creation class + commencement algo hitbox

---
+++ app/src/main/java/fr/dut/ptut2021/game/DrawOnIt.java
@@ -0,0 +1,52 @@
+package fr.dut.ptut2021.game;
+
+import fr.dut.ptut2021.models.Symbol;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DrawOnIt {
+
+    private Map<String, Symbol> symbols = new HashMap<>();
+
+    public DrawOnIt(){
+        symbols.put("0", new Symbol());
+        symbols.put("1", new Symbol());
+        symbols.put("2", new Symbol());
+        symbols.put("3", new Symbol());
+        symbols.put("4", new Symbol());
+        symbols.put("5", new Symbol());
+        symbols.put("6", new Symbol());
+        symbols.put("7", new Symbol());
+        symbols.put("8", new Symbol());
+        symbols.put("9", new Symbol());
+        symbols.put("A", new Symbol());
+        symbols.put("B", new Symbol());
+        symbols.put("C", new Symbol());
+        symbols.put("D", new Symbol());
+        symbols.put("E", new Symbol());
+        symbols.put("F", new Symbol());
+        symbols.put("G", new Symbol());
+        symbols.put("H", new Symbol());
+        symbols.put("I", new Symbol());
+        symbols.put("J", new Symbol());
+        symbols.put("K", new Symbol());
+        symbols.put("L", new Symbol());
+        symbols.put("M", new Symbol());
+        symbols.put("N", new Symbol());
+        symbols.put("O", new Symbol());
+        symbols.put("P", new Symbol());
+        symbols.put("K", new Symbol());
+        symbols.put("R", new Symbol());
+        symbols.put("S", new Symbol());
+        symbols.put("T", new Symbol());
+        symbols.put("U", new Symbol());
+        symbols.put("V", new Symbol());
+        symbols.put("W", new Symbol());
+        symbols.put("X", new Symbol());
+        symbols.put("Y", new Symbol());
+        symbols.put("Z", new Symbol());
+    }
+
+}
---
+++ app/src/main/java/fr/dut/ptut2021/models/Symbol.java
@@ -0,0 +1,67 @@
+package fr.dut.ptut2021.models;
+
+import java.util.ArrayList;
+
+
+public class Symbol {
+
+    private ArrayList<Point> points;
+    private int tolerance;
+
+    public Symbol(){
+        this.points = new ArrayList<>();
+
+    }
+
+    private int findIdOfNearestPoint(Point point){
+        int idNearestPoint;
+        if(points.size() == 0){
+            return -1;
+        }else {
+            int dist = (int) Math.sqrt(Math.pow(point.getX() - points.get(0).getX(), 2) + Math.pow(point.getY() - points.get(0).getY(), 2));
+            idNearestPoint = 0;
+            for (int i = 0; i < points.size(); i++) {
+                if ((int) Math.sqrt(Math.pow(point.getX() - points.get(i).getX(), 2) + Math.pow(point.getY() - points.get(i).getY(), 2)) <= dist) {
+                    idNearestPoint = i;
+                }
+            }
+        }
+        return idNearestPoint;
+    }
+
+    /*
+    public int findIdOfSecondNearestPoint(Point point){
+        int idNearestPoint = findIdOfNearestPoint(point);
+        if(points.size() <= 1){
+            return -1;
+        }else {
+            if(Math.sqrt(Math.pow(point.getX() - points.get(idNearestPoint + 1).getX(), 2) + Math.pow(point.getY() - points.get(idNearestPoint + 1).getY(), 2)) < Math.sqrt(Math.pow(point.getX() - points.get(idNearestPoint - 1).getX(), 2) + Math.pow(point.getY() - points.get(idNearestPoint - 1).getY(), 2))){
+                return idNearestPoint + 1;
+            }else{
+                return idNearestPoint - 1;
+            }
+        }
+    }
+    */
+
+    //p1 et p2 sont les limites de la zone
+    private boolean isInArea(Point point, Point p1, Point p2){
+        //todo
+        return true;
+    }
+
+    public boolean isInSymbol(Point point){
+
+        int idNearestPoint = findIdOfNearestPoint(point);
+        if(isInArea(point, points.get(idNearestPoint - 1), points.get(idNearestPoint))){
+            return true;
+        }
+        else if(isInArea(point, points.get(idNearestPoint), points.get(idNearestPoint + 1))){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
